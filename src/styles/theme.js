0380d2b5d9d4e3cee754db0cea2fd6cb2855dcb5
Jos3s <karatedkgt17@gmail.com>
2021-07-12 16:54:34 -0300

    Adicionado radii ao tema

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,11 @@
 import { fontSizes } from "./system/fontSizes";
 import { colors } from "./system/colors";
+import { space } from "./system/space";
 import { radii } from "./system/radii";
 
 export const theme = {
   colors,
   fontSizes,
   radii,
+  space,
 };
