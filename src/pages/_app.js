ae680b62c92d815d0f5fa6068fe8b1ab9004f322
Jos3s <karatedkgt17@gmail.com>
2021-07-13 11:11:31 -0300

    Removendo importações

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,3 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./../styles/global";
-import { theme, darkTheme } from "./../styles/theme";
-
 import { ThemeContextProvider } from "../contexts/ThemeContext";
 export default function App({ Component, pageProps }) {
   return (
