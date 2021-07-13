53000be5eeb5a5852753b0c2405902b24c22972b
Jos3s <karatedkgt17@gmail.com>
2021-07-13 11:09:37 -0300

    Criando a variante 'none' do Button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,16 +1,24 @@
 import styled from "styled-components";
-import { variant } from "styled-system";
+import { variant, layout, space, compose, background } from "styled-system";
 
 export const Button = styled.button`
-  ${variant({
-    variants: {
-      primary: {
-        color: "white",
-        backgroundColor: "element.primary",
-        borderRadius: "10000px",
-      },
-    },
-  })}
   border: 0;
   padding: 0.8rem 1.2rem;
+  ${compose(
+    variant({
+      variants: {
+        primary: {
+          color: "white",
+          backgroundColor: "element.primary",
+          borderRadius: "10000px",
+        },
+        none: {
+          color: "white",
+          background: "transparent",
+        },
+      },
+    }),
+    layout,
+    space
+  )}
 `;
