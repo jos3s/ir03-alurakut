ae33ecff42f066e7656ffde8437d43c4f7d474ac
Jos3s <karatedkgt17@gmail.com>
2021-07-13 22:20:38 -0300

    Removendo fundo do scroll

--- src/styles/global.js
+++ src/styles/global.js
@@ -40,9 +40,6 @@ export const GlobalStyle = createGlobalStyle`
   *::-webkit-scrollbar {
     width: 8px;
   }
-  *::-webkit-scrollbar-track {
-    background: #f1f1f1;
-  }
   *::-webkit-scrollbar-thumb {
     background: #888;
     border-radius: 10px;
