d00979a823f0998e786e8539fa05475e02062840
Jos3s <karatedkgt17@gmail.com>
2021-07-16 15:17:46 -0300

    Criando componentes estilizados

--- src/components/ItemBox/index.jsx
+++ src/components/ItemBox/index.jsx
@@ -3,10 +3,10 @@ import * as Styled from "./styles";
 export const ItemBox = ({ name, imgUrl, link }) => {
   return (
     <Styled.Item>
-      <a href={link ? link : `users/${name}`} key={name}>
-        <img src={imgUrl} alt={name} />
-        <span>{name}</span>
-      </a>
+      <Styled.Link href={link ? link : `users/${name}`} key={name}>
+        <Styled.Img src={imgUrl} alt={name} />
+        <Styled.Span>{name}</Styled.Span>
+      </Styled.Link>
     </Styled.Item>
   );
 };
--- src/components/ItemBox/styles.js
+++ src/components/ItemBox/styles.js
@@ -1,44 +1,46 @@
 import styled from "styled-components";
 
-export const Item = styled.li`
-  img {
-    object-fit: cover;
-    background-position: center center;
-    width: 100%;
-    height: 100%;
-    position: relative;
+export const Item = styled.li``;
+
+export const Img = styled.img`
+  object-fit: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+export const Link = styled.a`
+  display: inline-block;
+  height: 102px;
+  position: relative;
+  overflow: hidden;
+  border-radius: ${({ theme }) => theme.radii.md};
+  &:after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 1;
+    background-image: linear-gradient(0deg, #00000073, transparent);
   }
-  > a {
-    display: inline-block;
-    height: 102px;
-    position: relative;
-    overflow: hidden;
-    border-radius: ${({ theme }) => theme.radii.md};
-    span {
-      color: #ffffff;
-      font-size: 1rem;
-      position: absolute;
-      left: 0;
-      bottom: ${({ theme }) => theme.space.xsmall};
-      z-index: 2;
-      padding: 0 0.4rem;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      width: 100%;
-      display: -webkit-box;
-      -webkit-line-clamp: 3;
-      -webkit-box-orient: vertical;
-    }
-    &:after {
-      content: "";
-      display: block;
-      position: absolute;
-      top: 0;
-      right: 0;
-      left: 0;
-      bottom: 0;
-      z-index: 1;
-      background-image: linear-gradient(0deg, #00000073, transparent);
-    }
-  }
+`;
+
+export const Span = styled.span`
+  color: #ffffff;
+  font-size: 1rem;
+  position: absolute;
+  left: 0;
+  bottom: ${({ theme }) => theme.space.xsmall};
+  z-index: 2;
+  padding: 0 0.4rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
 `;
