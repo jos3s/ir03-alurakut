aabb525917bf0bd0f60028f48025214247e04d5e
Jos3s <karatedkgt17@gmail.com>
2021-07-16 15:18:03 -0300

    Usado githubUser

--- src/components/ProfileSideBar/index.jsx
+++ src/components/ProfileSideBar/index.jsx
@@ -8,7 +8,7 @@ import { Box } from "../Box";
 export const ProfileSideBar = ({ githubUser }) => {
   return (
     <Box position="sticky" top="10px">
-      <img src="https://github.com/jos3s.png" alt="Jos3s" />
+      <img src={`https://github.com/${githubUser}.png`} alt="Jos3s" />
       <hr />
       <Text my="medium">
         <BoxLink url={`https://github.com/${githubUser}`}>
