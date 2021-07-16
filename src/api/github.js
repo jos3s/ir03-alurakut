a22f080e6c590853c423a7832060a4f91ba03d83
Jos3s <karatedkgt17@gmail.com>
2021-07-16 00:17:17 -0300

    Atualizando funções da api do github

--- src/api/github.js
+++ src/api/github.js
@@ -1,9 +1,23 @@
 import axios from "axios";
 
+const USER = "jos3s";
+
+export const api = axios.create({
+  baseURL: "https://api.github.com/users/" + USER,
+});
+
 export const getFollowers = async () => {
-  const { data } = await axios.get(
-    "https://api.github.com/users/jos3s/followers"
-  );
+  const { data } = await api.get("/followers");
+  const newData = data.map((follower) => ({
+    name: follower.login,
+    imgUrl: follower.avatar_url,
+    login_url: follower.html_url,
+  }));
+  return newData;
+};
+
+export const getFollowing = async () => {
+  const { data } = await api.get("/following");
   const newData = data.map((follower) => ({
     name: follower.login,
     imgUrl: follower.avatar_url,
